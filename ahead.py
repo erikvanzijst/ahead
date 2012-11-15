c37b9584a45feab4a6c9300f3b83fafac0f88b14
Erik van Zijst <erik.van.zijst@gmail.com>
2012-11-14 16:31:12 -0800

    Terrible code, but it works.

--- ahead.py
+++ ahead.py
@@ -26,73 +26,108 @@ else:
     branches = r.branches()
 heads = set(itertools.chain(*[b.heads() for b in branches]))
 
-start = time.time()
-with r.walk(include=heads) as walker:
+class Done(Exception):
+    pass
 
-    b2c = {}
-    c2b = defaultdict(set)
-    for bh in [bh for bh in branches if bh != main]:
-        b2c[bh.name] = (set(bh.heads()), 0)
-        for head in bh.heads():
-            c2b[head].add(bh.name)
-    mcount = 0
+class Ahead(object):
+    def __init__(self, branches, main):
+        self.bmap = {bh.name: bh for bh in branches if bh != main}
+        self.b2c = {}
+        self.c2b = defaultdict(set)
+        for bh in [bh for bh in branches if bh != main]:
+            self.b2c[bh.name] = (set(bh.heads()), 0)
+            for head in bh.heads():
+                self.c2b[head].add(bh.name)
 
-    exclude = set(main.heads())
-    include = heads - exclude
+        self.exclude = set(main.heads())
+        self.include = heads - self.exclude
 
-    class Done(Exception):
-        pass
+    @property
+    def counts(self):
+        return {self.bmap[bname]: stats[1] for bname, stats in self.b2c.items()}
 
-    def ahead(cs):
-        if cs in exclude:
+    def visit(self, cs):
+        if cs in self.exclude:
             parents = cs.parents()
-            include.difference_update(parents)
-            exclude.remove(cs)
-            exclude.update(parents)
+            self.include.difference_update(parents)
+            self.exclude.remove(cs)
+            self.exclude.update(parents)
 
-            for b in c2b.pop(cs, tuple()):
-                parents, ahead = b2c[b]
+            for b in self.c2b.pop(cs, tuple()):
+                parents, ahead = self.b2c[b]
                 parents.remove(cs)
-                b2c[b] = (parents, ahead)
-            if not c2b:
+                self.b2c[b] = (parents, ahead)
+            if not self.c2b:
                 # all branches have been terminated
                 raise Done
 
-        elif cs in include:
-            include.remove(cs)
-            bnames = c2b.pop(cs)
-            liveparents = set(cs.parents()) - exclude
+        elif cs in self.include:
+            self.include.remove(cs)
+            bnames = self.c2b.pop(cs)
+            liveparents = set(cs.parents()) - self.exclude
 
             for b in bnames:
-                next, ahead = b2c[b]
+                next, ahead = self.b2c[b]
                 next.remove(cs)
                 next.update(liveparents)
-                b2c[b] = (next, ahead + 1)
+                self.b2c[b] = (next, ahead + 1)
 
             for p in liveparents:
-                c2b[p].update(bnames)
-            include.update(liveparents)
+                self.c2b[p].update(bnames)
+            self.include.update(liveparents)
         else:
             print 'WTF', cs.hash, cs.desc.split('\n')[:1][:30]
 
-    funcs = {ahead}
+class Behind(object):
+    def __init__(self, branch, main):
+        self.branch = branch
+        self.exclude = set(branch.heads())
+        self.include = set(main.heads()) - self.exclude
+        self.count = 0
+
+    def visit(self, cs):
+        if not self.include:
+            raise Done
+        else:
+            parents = cs.parents()
+            if self.exclude and cs in self.exclude:
+                self.include.difference_update(parents)
+                self.exclude.remove(cs)
+                # exclude its parents too:
+                self.exclude.update(parents)
+            elif cs in self.include:
+                self.count += 1
+                self.include.remove(cs)
+                for p in parents:
+                    if p not in self.exclude:
+                        self.include.add(p)
+
+
+start = time.time()
+with r.walk(include=heads) as walker:
+
+    ahead = Ahead(branches, main)
+    behinds = {}
+    for b in [b for b in branches if b != main]:
+        behinds[b] = Behind(b, main)
+
+    visitors = {ahead}
+    visitors.update(behinds.values())
     dead = set()
 
     for cs in walker:
-        if not funcs:
+        if not visitors:
             break
-        for func in funcs:
+        for visitor in visitors:
             try:
-                func(cs)
+                visitor.visit(cs)
             except Done:
-                dead.add(func)
+                dead.add(visitor)
 
         while dead:
-            funcs.remove(dead.pop())
+            visitors.remove(dead.pop())
 
 duration = time.time() - start
-for bname, stats in b2c.items():
-    print '%s [%d ahead] on %s' % (bname, stats[1], main.name)
-    if stats[0]:
-        print 'ERROR: parent nodes left for branch %s: %s' % (bname, stats[0])
+for b in [b for b in branches if b != main]:
+    print '%s [%d ahead] [%d behind] on %s' % (b.name, ahead.counts[b], behinds[b].count, main.name)
 print 'runtime: %.3f seconds' % duration
